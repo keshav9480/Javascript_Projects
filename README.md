6b7a805b293eca30f98453af1867790518c245c3
keshav-n-01 <p.keshav.n@gmail.com>
2021-04-15 09:46:56 +0530

    added drumkit code

--- README.md
+++ README.md
@@ -12,3 +12,28 @@ Modify/Update innerHTML using javascript
 
 etc...
 
+
+
+Higher Order Function
+These are the function which takes other functions as input parameter.
+Eg:
+
+function add(num1, num2) {
+return num1 + num2;
+}
+ 
+function subtract(num1, num2) {
+return num1 - num2;
+}
+ 
+function multiply(num1, num2) {
+return num1 * num2;
+}
+ 
+function divide(num1, num2) {
+return num1 / num2;
+}
+ 
+function calculator(num1, num2, operator) {
+return operator(num1, num2);
+}
--- app.js
+++ app.js
@@ -4,10 +4,48 @@ function changeColor(){
 	document.getElementsByClassName("btn")[0].style = "color:red";
 	setTimeout(document.getElementsByClassName("btn")[0].style.visibility = "hidden",10000);
 	setTimeout(document.getElementsByClassName("btn")[0].style.visibility = "visible", 1000);
-	
 }
 
 function qSelector() {
 	document.querySelector("h1").innerHTML = "<em>Basic JS</em>";
+}
+
+var numEle = document.querySelectorAll(".drum").length
+for( var i=0;i<numEle; i++){
+	document.querySelectorAll(".drum")[i].addEventListener('click', function (){
+		var ele = this.innerHTML;
+		console.log("element: "+ele);
+		switch(ele) {
+			case "w":
+				var audio = new Audio("sounds/crash.mp3");
+				audio.play();
+				break;
+			case "a":
+				var audio = new Audio("sounds/kick-bass.mp3");
+				audio.play();
+				break;
+			case "s":
+				var audio = new Audio("sounds/snare.mp3");
+				audio.play();
+				break;
+			case "d":
+				var audio = new Audio("sounds/tom-1.mp3");
+				audio.play();
+				break;
+			case "j":
+				var audio = new Audio("sounds/tom-2.mp3");
+				audio.play();
+				break;
+			case "k":
+				var audio = new Audio("sounds/tom-3.mp3");
+				audio.play();
+				break;
+			case "l":
+				var audio = new Audio("sounds/tom-4.mp3");
+				audio.play();
+				break;
+			default:;
+		}
+	});
 }
 
